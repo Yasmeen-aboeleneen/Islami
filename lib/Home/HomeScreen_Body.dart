f8af694ec2dbd7479dea6e91b0ae47743fc1849e
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-09-03 02:55:08 +0300

    Start in Home Screen

--- lib/Home/HomeScreen_Body.dart
+++ lib/Home/HomeScreen_Body.dart
@@ -10,6 +10,23 @@ class HomeScreenBody extends StatefulWidget {
 class _HomeScreenBodyState extends State<HomeScreenBody> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+         
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 80,),
+
+          Center(
+            child: Text('اسلامي',style: TextStyle(
+              fontSize: 80,
+              fontFamily: '18 Khebrat Musamim Bold.ttf',
+              color: Color.fromARGB(255, 226, 204, 4)
+            ),),
+          ),
+       
+        ],
+      ),
+    );
   }
 }
